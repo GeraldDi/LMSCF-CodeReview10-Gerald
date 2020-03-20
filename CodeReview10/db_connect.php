1b4a7a55106df01de1f4458d3e8d78d5372b1032
GeraldDi <59823625+GeraldDi@users.noreply.github.com>
2020-03-20 09:32:36 +0100

    init

---
+++ CodeReview10/db_connect.php
@@ -0,0 +1,18 @@
+<?php 
+
+$localhost = "127.0.0.1";
+$username = "root";
+$password = "";
+$dbname = "cr10_gerald_biglibrary";
+
+
+$connect = new  mysqli($localhost, $username, $password, $dbname);
+
+
+if($connect->connect_error) {
+    die("connection failed: " . $connect->connect_error);
+} else {
+    echo "Successfully Connected";
+}
+
+?>
